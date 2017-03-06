9c37b552c17933599c92bbfd10ee82d732654a6a
Fanchao Zhou <fanchaozhou1992@gmail.com>
2017-03-06 16:36:18 -0600

    GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ var bodyParser = require('body-parser');
 var mongoose = require('./db/mongoose.js').mongoose;
 var Todo = require('./models/todo.js').Todo;
 var User = require('./models/user.js').User;
+var ObjectID = require('mongodb').ObjectID;
 
 var app = express();
 
@@ -24,10 +25,32 @@ app.post('/todos', (req, res) => {
 
 app.get('/todos', (req, res) => {
 	Todo.find().then((todos) => {
-		res.send({todos});
+		res.send({
+			todos
+		});
 	}).catch((err) => {
 		res.status(400).send(err);
 	});
+});
+
+app.get('/todos/:id', (req, res) => {
+	var id = req.params.id;
+	
+	if(ObjectID.isValid(id)) {
+		Todo.findById(id)
+			.then((todo) => {
+				if(todo) {
+					res.send({todo});
+				} else {
+					res.status(404).send({});
+				}
+			})
+			.catch((e) => {
+				res.status(400).send({});
+			});
+	} else {
+		res.status(404).send({});
+	}
 });
 
 app.listen(3000, () => {
